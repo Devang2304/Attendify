a339bbb3dc66bbc00e9172f9e79f4f9b93309cc9
Devang2304 <devangvartak2304@gmail.com>
2023-05-31 00:31:31 +0530

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Login from "./Components/login/Login";
 import Signup from "./Components/signup/Signup";
+import HomePage from "./Components/homePage/Homepage"
 
 const App =()=>{
     return(
         <div>
             {/* <Login/> */}
-            <Signup/>
+            {/* <Signup/> */}
+            <HomePage/>
         </div>
     )
 }
