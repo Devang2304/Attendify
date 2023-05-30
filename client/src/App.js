4f44dfc8b8d3c50026bb5a5b575fdc1278e9723a
Devang2304 <devangvartak2304@gmail.com>
2023-05-30 22:39:11 +0530

    basic structure

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "./Components/login/Login";
+import Signup from "./Components/signup/Signup";
+
+const App =()=>{
+    return(
+        <div>
+            {/* <Login/> */}
+            <Signup/>
+        </div>
+    )
+}
+
+export default App;
