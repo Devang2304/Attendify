89c03857ec714a96951053300b7411860280abb5
Devang2304 <devangvartak2304@gmail.com>
2023-05-31 00:27:37 +0530

    home page UI

---
+++ client/src/Components/homePage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './homePage.css';
+
+const HomePage = () =>{
+    return(
+        <div>
+            <h2>Welcome to<br/>Attendance Tracker</h2>
+        </div>
+    )
+}
+
+export default HomePage;
