aa7fb70b0d89f7784cabe9539c913e9c047e85b6
Devang2304 <devangvartak2304@gmail.com>
2023-06-01 02:15:15 +0530

    added attendance form

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import Login from "./Components/login/Login";
 import Signup from "./Components/signup/Signup";
 import HomePage from "./Components/homePage/Homepage"
+import AttendanceForm from "./Components/attendanceForm/AttendanceForm";
 
 const App =()=>{
     return(
         <div>
-            <Login/>
+
+            {/* <Login/> */}
             {/* <Signup/> */}
             {/* <HomePage/> */}
+            <AttendanceForm/>
         </div>
     )
 }
