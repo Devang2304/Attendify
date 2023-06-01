e04c3bc9bd1a5945f1374dd9a717f263e6436016
Devang2304 <devangvartak2304@gmail.com>
2023-06-02 02:40:11 +0530

    partial routing done

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,18 @@ import Login from "./Components/login/Login";
 import Signup from "./Components/signup/Signup";
 import HomePage from "./Components/homePage/Homepage"
 import AttendanceForm from "./Components/attendanceForm/AttendanceForm";
-
+import {BrowserRouter , Route, Routes} from 'react-router-dom';
 const App =()=>{
     return(
-        <div>
-
-            {/* <Login/> */}
-            {/* <Signup/> */}
-            {/* <HomePage/> */}
-            <AttendanceForm/>
-        </div>
-    )
+       <BrowserRouter>
+       <Routes>
+       <Route path="/" element={<HomePage/>}/>
+       <Route path="/login" element={<Login/>}/>
+       <Route path="signup" element={<Signup/>}/>
+       <Route path="edit" element={<AttendanceForm/>}/>
+       </Routes>
+       </BrowserRouter>
+    );
 }
 
 export default App;
