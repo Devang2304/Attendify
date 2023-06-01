6f96c828f47df5284ed82896f800b3768d3476fa
Devang2304 <devangvartak2304@gmail.com>
2023-06-02 02:38:43 +0530

    linked in login page

--- client/src/Components/signup/Signup.jsx
+++ client/src/Components/signup/Signup.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import './signup.css';
-
+import { useNavigate } from "react-router-dom";
 const Signup = () => {
+    const navigate = useNavigate();
     return (
         <div className="box">
             <span className="borderLine"></span>
             <form action="">
                 <h2>Sign up</h2>
                 <div className="inputBox">
-                <input type="text"  required="required" />
+                <input type="text"  required />
                 <span>Create Username</span>
                 <i></i>
                 </div>
                 <div className="inputBox">
-                    <input type="password" required="required" />
+                    <input type="password" required />
                     <span>Create Password</span>
                     <i></i>
                 </div>
                 <div className="links">
-                    <a href="#">Sign up</a>
+                    <button className="btsign" onClick={()=>(navigate('/login'))}>Login</button>
                 </div>
                 <input type="submit" value="Sign up" />
             </form>
