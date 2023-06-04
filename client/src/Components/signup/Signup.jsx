1ea3a6dc1c94749102370ac8ae6428094d0e0cd6
Devang2304 <devangvartak2304@gmail.com>
2023-06-04 19:26:19 +0530

    Preparing for Backend 🔥

--- client/src/Components/signup/Signup.jsx
+++ client/src/Components/signup/Signup.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React,{useState} from "react";
 import './signup.css';
 import { useNavigate } from "react-router-dom";
+
+
 const Signup = () => {
+
     const navigate = useNavigate();
+    const [user,setUser] = useState({
+        userName:"",
+        password:""
+    });
+
+    const handleChange =(e)=>{
+        const {name,value} = e.target;
+        setUser({
+            ...user,
+            [name]:value
+        })
+        console.log("User Registration:",user);
+    }
+
     return (
         <div className="box">
             <span className="borderLine"></span>
             <form action="">
                 <h2>Sign up</h2>
                 <div className="inputBox">
-                <input type="text"  required />
+                <input type="text" name="userName" value={user.userName} onChange={handleChange}  required />
                 <span>Create Username</span>
                 <i></i>
                 </div>
                 <div className="inputBox">
-                    <input type="password" required />
+                    <input type="password" name="password" value={user.password} onChange={handleChange}  required />
                     <span>Create Password</span>
                     <i></i>
                 </div>
