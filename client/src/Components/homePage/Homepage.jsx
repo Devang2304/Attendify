59e9450dd9082817874e7590ba888e6dc750a2b6
Devang2304 <devangvartak2304@gmail.com>
2023-06-04 18:10:07 +0530

    done some routing

--- client/src/Components/homePage/Homepage.jsx
+++ client/src/Components/homePage/Homepage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import './homePage.css';
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () =>{
+    const navigate =useNavigate();
     return(
-        <div className="homePage">
-            <h2>Welcome to<br/>Attendance Tracker</h2>
+        <div>
+            <h2 className="homePage">Welcome to<br/>Attendance Tracker</h2>
+            <br /><br /><br /><br /><br />
+            <div className="getStarted"><button onClick={()=>(navigate('/login'))}><span>Track Now→</span></button></div>
         </div>
     )
 }
