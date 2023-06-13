e7844a0a9c2c7f5e55b2bca5f7bf9271dad59ec2
Devang2304 <devangvartak2304@gmail.com>
2023-06-13 15:37:16 +0530

    created user & attendance controller

---
+++ server/controller/userController.js
@@ -0,0 +1,11 @@
+import User from '../model/user.js'
+
+
+export const loginUser =(req, res) =>{
+    res.json({message: 'login User'});
+}
+
+export const registerUser =(req, res) =>{
+    res.json({message: 'Register User'});
+}
+
