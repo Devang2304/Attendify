6b27c457dbeab592c55198abde20ff9e48d8e6bb
Devang2304 <devangvartak2304@gmail.com>
2023-06-13 15:41:54 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ const App =()=>{
        <Route path="/" element={<HomePage/>}/>
        <Route path="/login" element={<Login/>}/>
        <Route path="signup" element={<Signup/>}/>
-       <Route path="edit" element={<AttendanceForm/>}/>
+       <Route path="/edit" element={<AttendanceForm/>}/>
        </Routes>
        </BrowserRouter>
     );
