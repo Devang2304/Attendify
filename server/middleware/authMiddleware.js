2c5f74343af7ab3418b98a64a519cf36e8a2dff9
Devang2304 <devangvartak2304@gmail.com>
2023-06-14 20:47:01 +0530

    added auth (protected routes)

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+import User from '../model/user.js'
+
+
+export const protect = async (req, res, next) => {
+    let token;
+
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        try {
+            // get token from header
+        token = req.headers.authorization.split(' ')[1];
+        // verfiy token
+        const decoded = jwt.verify(token,process.env.JWT_SECRET);
+
+        // get user from the token
+        req.user = await User.findById(decoded.id).select('-password')
+
+        next();
+
+        } catch (error) {
+            console.log(error);
+            res.status(401).send('Not authorized');
+        }
+    }
+    if(!token){
+        res.status(401).send('Not authorized, No token created');
+    }
+}
