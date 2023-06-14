00c85b87d7a52321c39e2534a1f51eb9ea452ca6
Devang2304 <devangvartak2304@gmail.com>
2023-06-14 20:45:47 +0530

    testing

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {loginUser,registerUser} from '../controller/userController.js';
+import {loginUser,registerUser,getMe} from '../controller/userController.js';
+import {protect} from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.post('/login',loginUser);
 router.post('/register',registerUser);
-
+router.get('/me', protect ,getMe);
 export default router;
