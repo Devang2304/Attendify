65c1525ccfaa41ededb93eb599b73c5c4e215bb2
Devang2304 <devangvartak2304@gmail.com>
2023-10-16 00:30:40 +0530

    created redux reducers for getAttendances,delete,getSingle and edit but edit reducer => not working

--- client/src/features/attendance/attendanceService.js
+++ client/src/features/attendance/attendanceService.js
@@ -12,9 +12,54 @@ const createAttendance = async (UserAttendanceData, token) => {
     const response = await axios.post(`${API_URL}/add`,UserAttendanceData,config);
     return response.data;
 }
+//get all attendances data
+const getAttendances = async (token) => {
+    const config = {
+        headers: {
+            Authorization : `Bearer ${token}`
+        }
+    }
+    const response = await axios.get(`${API_URL}/attendance`,config);
+    return response.data;
+}
+//get single attendances data
+const getSingleAttendance = async (id,token) => {
+    const config = {
+        headers: {
+            Authorization : `Bearer ${token}`
+        }
+    }
+    const response = await axios.get(`${API_URL}/${id}`,config);
+    return response.data;
+}
+//get single attendances data
+const editAttendance = async (id,EditedattendData,token) => {
+    const config = {
+        headers: {
+            Authorization : `Bearer ${token}`
+        }
+    }
+    const response = await axios.post(`${API_URL}/${id}`,config,EditedattendData);
+    return response.data;
+}
+
+// delete attendance
+const deleteAttendance = async (id,token) => {
+    const config = {
+        headers: {
+            Authorization : `Bearer ${token}`
+        }
+    }
+    const response = await axios.delete(`${API_URL}/${id}`,config);
+    return response.data;
+}
 
 const attendanceService = {
     createAttendance,
+    getAttendances,
+    deleteAttendance,
+    getSingleAttendance,
+    editAttendance
 }
 export default attendanceService;
 
--- client/src/features/attendance/attendanceSlice.js
+++ client/src/features/attendance/attendanceSlice.js
@@ -23,6 +23,58 @@ export const createAttendance = createAsyncThunk('/add', async (UserAttendanceDa
 })
 
 
+// getattnedances
+export const getAttendances = createAsyncThunk('/attendance', async (_, thunkAPI) =>{
+    try {
+        const token = thunkAPI.getState().auth.user.token;
+        return await attendanceService.getAttendances(token);
+    } catch (error) {
+        const message = 
+        (error.response && error.response.data && error.response.data.message) ||
+        error.message || error.toString()
+        return thunkAPI.rejectWithValue(message);
+    }
+})
+
+// get single subject attendance
+export const getSingleAttendance = createAsyncThunk('/getSingleAttendance', async (id, thunkAPI) =>{
+    try {
+        const token = thunkAPI.getState().auth.user.token;
+        return await attendanceService.getSingleAttendance(id,token);
+    } catch (error) {
+        const message = 
+        (error.response && error.response.data && error.response.data.message) ||
+        error.message || error.toString()
+        return thunkAPI.rejectWithValue(message);
+    }
+})
+// edit single subject attendance
+export const editAttendance = createAsyncThunk('/editAttendance', async (id,EditedattendData,thunkAPI) =>{
+    try {
+        const token = thunkAPI.getState().auth.user.token;
+        return await attendanceService.editAttendance(id,EditedattendData,token);
+    } catch (error) {
+        const message = 
+        (error.response && error.response.data && error.response.data.message) ||
+        error.message || error.toString()
+        return thunkAPI.rejectWithValue(message);
+    }
+})
+
+// Delete subject attendance
+export const deleteAttendance = createAsyncThunk('/delete', async (id, thunkAPI) =>{
+    try {
+        const token = thunkAPI.getState().auth.user.token;
+        return await attendanceService.deleteAttendance(id,token);
+    } catch (error) {
+        const message = 
+        (error.response && error.response.data && error.response.data.message) ||
+        error.message || error.toString()
+        return thunkAPI.rejectWithValue(message);
+    }
+})
+
+
 export const attendanceSlice = createSlice({
     name: 'attendance',
     initialState,
@@ -41,6 +93,66 @@ export const attendanceSlice = createSlice({
 
         })
         .addCase(createAttendance.rejected, (state,action) => {
+            state.isLoading= false;
+            state.isError = true;
+            state.message = action.payload;
+
+        })
+        .addCase(getAttendances.pending, ( state ) =>{
+            state.isLoading = true;
+        })
+        .addCase(getAttendances.fulfilled, (state,action) => {
+            state.isLoading= false;
+            state.isSuccess = true;
+            state.attendance = action.payload;
+
+        })
+        .addCase(getAttendances.rejected, (state,action) => {
+            state.isLoading= false;
+            state.isError = true;
+            state.message = action.payload;
+
+        })
+        .addCase(deleteAttendance.pending, ( state ) =>{
+            state.isLoading = true;
+        })
+        .addCase(deleteAttendance.fulfilled, (state,action) => {
+            state.isLoading= false;
+            state.isSuccess = true;
+            state.attendance = state.attendance.filter((attendance)=>attendance._id !== action.payload.id);
+
+        })
+        .addCase(deleteAttendance.rejected, (state,action) => {
+            state.isLoading= false;
+            state.isError = true;
+            state.message = action.payload;
+
+        })
+        .addCase(getSingleAttendance.pending, ( state ) =>{
+            state.isLoading = true;
+        })
+        .addCase(getSingleAttendance.fulfilled, (state,action) => {
+            state.isLoading= false;
+            state.isSuccess = true;
+            state.attendance = action.payload;
+
+        })
+        .addCase(getSingleAttendance.rejected, (state,action) => {
+            state.isLoading= false;
+            state.isError = true;
+            state.message = action.payload;
+
+        })
+        .addCase(editAttendance.pending, ( state ) =>{
+            state.isLoading = true;
+        })
+        .addCase(editAttendance.fulfilled, (state,action) => {
+            state.isLoading= false;
+            state.isSuccess = true;
+            state.attendance = action.payload;
+
+        })
+        .addCase(editAttendance.rejected, (state,action) => {
             state.isLoading= false;
             state.isError = true;
             state.message = action.payload;
